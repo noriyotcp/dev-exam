44b1c8001eb9b49ef3a6294c79cd8012c9ca89a6
noriyotcp <noriyo.akita@gmail.com>
2016-08-02 08:53:13 +0900

    modelのテスト

--- test/models/apartment_test.rb
+++ test/models/apartment_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ApartmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @apartment = apartments(:one)
+  end
+
+  test "should be valid" do
+    assert @apartment.valid?
+  end
+
+  test "name should be present" do
+    @apartment.name = nil
+    assert_not @apartment.valid?
+  end
+
+  test "rent should be present" do
+    @apartment.rent = nil
+    assert_not @apartment.valid?
+  end
+
+  test "address should be present" do
+    @apartment.address = nil
+    assert_not @apartment.valid?
+  end
+
+  test "building_age should be present" do
+    @apartment.building_age = nil
+    assert_not @apartment.valid?
+  end
+
+  test "rent should be greater than or equal to 0" do
+    @apartment.rent = -1
+    assert_not @apartment.valid?
+  end
+
+  test "rent should be integer" do
+    @apartment.rent = 10000.111
+    assert_not @apartment.valid?
+  end
 end
--- test/models/nearest_station_test.rb
+++ test/models/nearest_station_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class NearestStationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @nearest_station = nearest_stations(:one)
+  end
+
+  test "should be valid" do
+    assert @nearest_station.valid?
+  end
+
+  test "minutes_walk should be greater than or equal to 0" do
+    @nearest_station.minutes_walk = -1
+    assert_not @nearest_station.valid?
+  end
+
+  test "minutes_walk should be integer" do
+    @nearest_station.minutes_walk = 5.5
+    assert_not @nearest_station.valid?
+  end
 end
