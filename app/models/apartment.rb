04b4660c061fd62f57aa4001c73f38e1bab918a5
noriyotcp <noriyo.akita@gmail.com>
2016-08-02 05:24:45 +0900

    Apartmentにvalidation追加

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,5 +3,8 @@ class Apartment < ApplicationRecord
 
   accepts_nested_attributes_for :nearest_stations
 
+  validates :name, :rent, :address, :building_age, presence: true
+  validates_numericality_of :rent, :building_age, greater_than_or_equal_to: 0, only_integer: true
+
   NEAREST_STATIONS_COUNT = 2
 end
