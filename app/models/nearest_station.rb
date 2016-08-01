51d6f93d4329d1f9b7c13239f8a21e4db90b6286
noriyotcp <noriyo.akita@gmail.com>
2016-08-02 08:52:32 +0900

    NearestStationにvalidation追加
    minutes_walkがintegerで0以上、allow_nil: true

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,5 @@
 class NearestStation < ApplicationRecord
   belongs_to :apartment, optional: true
+
+  validates_numericality_of :minutes_walk, greater_than_or_equal_to: 0, only_integer: true, allow_nil: true
 end
